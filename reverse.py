84753cb1794c9f0b58939baa492601ddc4532be7
ravindrakumarsaini <raveensbsaini@gmail.com>
2025-01-02 21:44:36 +0530

    addin g reverse.py

---
+++ reverse.py
@@ -0,0 +1,6 @@
+def reverseString(string):
+    return string[::-1]
+assert reverseString('Hello') == 'olleH'
+assert reverseString('') == ''
+assert reverseString('aaazzz') == 'zzzaaa'
+assert reverseString('xxxx') == 'xxxx'
