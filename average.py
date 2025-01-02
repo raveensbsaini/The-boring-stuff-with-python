d8d5d7dadc4f0b554d4715e5c4d313d6b4ebc937
ravindrakumarsaini <raveensbsaini@gmail.com>
2025-01-02 22:09:03 +0530

    adding average.py

---
+++ average.py
@@ -0,0 +1,16 @@
+def average(list1):
+    sum = 0
+    for i in list1:
+        sum += i
+    return sum / len(list1)
+    
+assert average([1, 2, 3]) == 2
+assert average([1, 2, 3, 1, 2, 3, 1, 2, 3]) == 2
+assert average([12, 20, 37]) == 23
+assert average([0, 0, 0, 0, 0]) == 0
+import random
+random.seed(42)
+testData = [1, 2, 3, 1, 2, 3, 1, 2, 3]
+for i in range(1000):
+    random.shuffle(testData)
+    assert average(testData) == 2
