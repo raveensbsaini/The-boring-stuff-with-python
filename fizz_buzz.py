5ad5b1f9c0bf7d5ee73c221489658a35f4523017
ravindrakumarsaini <raveensbsaini@gmail.com>
2025-01-05 11:49:14 +0530

    adding leap_year.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,10 +1,22 @@
+def fizzBuzzAnkit(upTo: int):
+    for i in range(1, upTo + 1):
+        if i % 3 == 0 and i % 5 == 0:
+           print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+           print("Fizz", end=" ")
+        elif i % 5 == 0:
+          print("Fizz", end=" ")
+        else:
+            print(str(i), end=" ")
+
+
 def fizzBuzz(upTo):
     for i in range(1,upTo+1):
         # print("i",i)
         run(i)
 def run(i):
     divisible_by_3 = False
-    divisible_by_5 = False        
+    divisible_by_5 = False
     if i //3 == i/3 :
         divisible_by_3 = True
     if i //5 == i/5 :
@@ -13,20 +25,20 @@ def run(i):
     # print("divisible_by_5",divisible_by_5)
     if divisible_by_3 and  divisible_by_5:
         # print("both 3 and 5")
-        print("FizzBuzz ",end="") 
+        print("FizzBuzz ",end="")
         return None
     if divisible_by_3 == False and divisible_by_5 == False:
         # print("both 3, and 5 none")
-        print(i," ",end="")  
+        print(i," ",end="")
         return None
     if (divisible_by_3 == False) and  divisible_by_5 == True:
         # print("only 3 false 5 true")
 
-        print("Fizz ",end="") 
+        print("Fizz ",end="")
         return
     if divisible_by_3 and  (divisible_by_5 == False):
         # print("only 3 true")
-        print("Buzz ",end="") 
+        print("Buzz ",end="")
         return
 fizzBuzz(35)
 print()
---
+++ validate_date.py
@@ -0,0 +1,46 @@
+def isLeapYear(year):
+    if year % 4 == 0:
+        if year % 100  == 0 and year % 400 == 0:
+            return True
+        elif year % 100 == 0 and year % 400 != 0:
+            return False
+        else:
+            return True
+    else:
+        return False
+def isValidDate(year,month,day):
+    months = {1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
+    if 0<= month <= 12 and 0<= day <= 31:
+        if isLeapYear(year) :
+            if month == 2:
+                if 0<= day <= 29:
+                    return True
+                else:
+                    return False
+        for i in d:
+            if month == i:
+                if 0<= day <= d[i]:
+                    return True
+                else:
+                    return False
+        
+    else:
+        return False
+        
+    
+
+assert isValidDate(1999, 12, 31) == True
+assert isValidDate(2000, 2, 29) == True
+assert isValidDate(2001, 2, 29) == False
+assert isValidDate(2029, 13, 1) == False
+assert isValidDate(1000000, 1, 1) == True
+assert isValidDate(2015, 4, 31) == False
+assert isValidDate(1970, 5, 99) == False
+assert isValidDate(1981, 0, 3) == False
+assert isValidDate(1666, 4, 0) == False
+import datetime
+d = datetime.date(1970, 1, 1)
+oneDay = datetime.timedelta(days=1)
+for i in range(1000000):
+    assert isValidDate(d.year, d.month, d.day) == True
+    d += oneDay
