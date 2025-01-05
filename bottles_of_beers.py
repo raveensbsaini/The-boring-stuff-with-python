2c815d8e86e72cc02afed26281c5afddbf208b95
ravindrakumarsaini <raveensbsaini@gmail.com>
2025-01-05 13:12:45 +0530

    updating bottles_and_beers.py

--- bottles_of_beers.py
+++ bottles_of_beers.py
@@ -3,8 +3,13 @@ def song():
 {a} bottles of beer,
 Take one down,
 Pass it around,"""
-    for i in range(99,0,-1):
+    for i in range(99,1,-1):
         print(string.format(a=i))
+    print("""1 bottle of beer on the wall,
+1 bottle of beer,
+Take one down,
+Pass it around,"""
+)
     print("No more bottles of beer on the wall!")
     return None
 song()
